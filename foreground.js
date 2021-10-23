2f51545f0ea0f8015a657420da0fde2cadb8ff45
kyupid <iamscvv@gmail.com>
2021-10-23 23:38:52 +0900

    웹페이지에 버튼만들어보기

--- foreground.js
+++ foreground.js
@@ -1,1 +1,12 @@
-document.querySelector(".rSk4se img").classList.add("spinspinspin");
+// document.querySelector(".rSk4se img").classList.add("spinspinspin");
+
+const first = document.createElement("button");
+first.innerText = "SET DATA";
+first.id = "'first'";
+
+const second = document.createElement("button");
+second.innerText = "SHOUTOUT TO BACKEND";
+second.id = "second";
+
+document.querySelector("body").appendChild(first);
+document.querySelector("body").appendChild(second);
