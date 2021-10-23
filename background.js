d4bf7f54d0c7ac937dfeeb51a882b024a1c85892
kyupid <iamscvv@gmail.com>
2021-10-23 22:46:09 +0900

    js 주입 테스트

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        console.log(current_tab_info.url);
-    })
+chrome.tabs.onActivated.addListener((tab) => {
+  chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
+        console.log("i injected")
+      );
+    }
+  });
 });
-
-// chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('test'))
