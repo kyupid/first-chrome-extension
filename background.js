0dabf01aae150e4eac03e6b1d2798eae769224fa
kyupid <iamscvv@gmail.com>
2021-10-23 22:40:05 +0900

    탭 옮길때마다 URL 따오기

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab)
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        console.log(current_tab_info.url);
+    })
 });
 
 // chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('test'))
