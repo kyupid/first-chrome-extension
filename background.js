17bf91b491997d7f705e2d177dbc5abfb2ccbcdc
kyupid <iamscvv@gmail.com>
2021-10-23 22:36:01 +0900

    탭 옮길 때마다 background 이 listening

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
-chrome.tabs.executeScript(null, {file: './foreground.js'}, ()=>console.log('test'))
+chrome.tabs.onActivated.addListener(tab => {
+    console.log(tab)
+});
+
+// chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('test'))
