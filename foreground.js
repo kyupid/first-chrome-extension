b07212198f15bda838122d6dcb4270378285e190
kyupid <iamscvv@gmail.com>
2021-10-23 23:40:59 +0900

    Type

--- foreground.js
+++ foreground.js
@@ -2,7 +2,7 @@
 
 const first = document.createElement("button");
 first.innerText = "SET DATA";
-first.id = "'first'";
+first.id = "first";
 
 const second = document.createElement("button");
 second.innerText = "SHOUTOUT TO BACKEND";
